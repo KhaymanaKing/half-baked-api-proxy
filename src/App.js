9b4c38073f015476989835212b891fe9b9eb419f
Khayman King <khaymanak@gmail.com>
2022-05-18 11:32:05 -0700

    adding spotify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import PokemonSearch from './PokemonSearch';
 import YelpSearch from './YelpSearch';
 import WeatherSearch from './WeatherSearch';
+import Spotify from './Spotify';
 import './App.css';
 
+
 function App() {
+  const data = [
+    { value: 1, name: 'A' },
+    { value: 2, name: 'B' },
+    { value: 3, name: 'C' }
+  ];
   return (
     <div className="App">
       <PokemonSearch />
       <YelpSearch />
       <WeatherSearch /> 
+      <Spotify/>
     </div>
   );
 }
